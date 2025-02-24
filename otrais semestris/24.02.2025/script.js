24a39015914df0edee55c041c79daf275b41b0f5
arseny13-52 <nikolavanaga13@gmail.com>
2025-02-24 11:21:46 +0200

    24.02.2025

---
+++ otrais semestris/24.02.2025/script.js
@@ -0,0 +1,48 @@
+let a = 5;
+let b = "5";
+
+if( a == b )
+{
+    console.log("a == b");
+}
+if( a == b )
+{
+    console.log("a == b");
+}
+
+
+
+/*
+let diena = parseInt(promt("Ievadi dienas numuru"));
+
+switch( diena )
+{
+        case 1:
+        console.log("pirmdiena");
+        break;
+
+        case 2:
+        console.log("otrdiena");
+        break;
+
+        case 3:
+        console.log("trešdiena");
+        break;
+
+        case 4:
+        console.log("ceturtdiena");
+        break;
+
+        case 5:
+        console.log("piektdiena");
+        break;
+
+        case 6:
+        console.log("sestdiena");
+        break;
+
+        case 7:
+        console.log("svētdiena");
+        break;
+}
+*/
