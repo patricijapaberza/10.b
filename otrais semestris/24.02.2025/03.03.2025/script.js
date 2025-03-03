b4dc184353337a45fa0f52f228b5d76718f13b8e
arseny13-52 <nikolavanaga13@gmail.com>
2025-03-03 11:18:35 +0200

    03.03.2025

---
+++ otrais semestris/24.02.2025/03.03.2025/script.js
@@ -0,0 +1,61 @@
+/*
+let masivs1 = [1,2,3,4,5];
+let masivs2 = []
+
+console.log(masivs1);
+console.log(masivs1.length);
+console.log(masivs1[0]);
+console.log(masivs[1]);
+console.log(masivs1[2]);
+console.log(masivs1[3]);
+console.log(masivs1[4]);
+let x = 4;
+console.log(masivs1[x]);
+let i =masivs1.length - 1;
+console.log(masivs1[i])
+*/
+
+/*
+let arr = ["q","w","e","r","t","y"];
+console.log(arr);
+arr.push("u");
+console.log(arr);
+
+
+arr.unshift("Tab");
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+
+arr[2] = null;
+console.log(arr);
+*/
+
+/*
+for (let i = 5; i >=0 ; i--)
+{
+    console.log(i);
+}
+*/
+
+
+
+
+/*
+let arr = [1,2,3,4,5,6,7,8,9,10];
+for ( let i = 0 ; i < arr.length ; i++)
+{
+    console.log(arr[i]);
+}
+*/
+
+let arr = [0,1,2,3,4];
+for ( let i = 0 ; i <= 4 ; i++)
+{
+    console.log(arr[i]);
+}
