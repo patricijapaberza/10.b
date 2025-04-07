ba6ff068bbfc9fb0c7d1baeb91deb76b0f4b089b
arseny13-52 <nikolavanaga13@gmail.com>
2025-04-07 11:20:45 +0300

    07.04.2025

---
+++ otrais semestris/KD02/script.js
@@ -0,0 +1,9 @@
+function myFunction()
+{
+    let user_vards = document.getElementById("input_vards");
+    let user_vecums = document.getElementById("input_vecums");
+    console.log(user_vards.value);
+    console.log(user_vecums.value);
+    vecums >= 18
+    irPilsonis == true
+}
