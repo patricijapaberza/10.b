afdd461165e06355f38ed79b67e637bbf88cab26
patricijapaberza <paberzapatricija@gmail.com>
2025-04-28 11:20:17 +0300

    28.04.2025

--- otrais semestris/28.04.2025/script.js
+++ otrais semestris/28.04.2025/script.js
@@ -4,11 +4,28 @@ function myFunction1()
     const surnameObj = document.getElementById("user_surname");
     const urlObj = document.getElementById("user_url");
 
-    console.log(nameObj.value);
-    console.log(surnameObj.value);
-    console.log(urlObj.value);
+    //console.log(nameObj.value);
+    //console.log(surnameObj.value);
+    //console.log(urlObj.value);
 
-    localStorage.setItem("name", nameObj.value);
-    ocalStorage.setItem("surname", surnameObj.value);
-    ocalStorage.setItem("url", urlObj.value);
+    //localStorage.setItem("name", nameObj.value);
+    //ocalStorage.setItem("surname", surnameObj.value);
+    //ocalStorage.setItem("url", urlObj.value);
+
+    const user =
+    {
+        name : nameObj.value,
+        surname : surnameObj.value,
+        url : urlObj.value
+    }
+    console.log(user.name);
+    localStorage.setItem("user1",JSON.stringify(user));
+
+}
+
+function myFunction2()
+{
+    const user = JSON.parse(localStorage.getItem("user1"));
+    console.log(user);
+    alert(user.name + user.surname + user.url);
 }
